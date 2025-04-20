5330643ccfe5518efeca1aff41b4dbd1bc74f8ee
Joemar Aguinea <s.aguinea.joemar@cmu.edu.ph>
2025-04-20 14:28:34 +0800

    Update ProjectsCard.js

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -58,9 +58,10 @@ const ProjectsCard = ({ title, des, src, github, website, figma }) => {
               )}
             </div>
           </div>
-          <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
+          <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300 text-justify">
             {des}
           </p>
+
         </div>
       </div>
     </div>
