42e2cf7ace3667e3c81e1c37d80c636515be07e2
Joemar Aguinea <s.aguinea.joemar@cmu.edu.ph>
2026-07-17 23:13:20 +0800

    Update skills section with new technical skills

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -8,15 +8,82 @@ const Skills = () => {
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
       className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
     >
+      
       <div className="w-full lgl:w-1/2">
         <div className="py-12 font-titleFont flex flex-col gap-4">
           {/* <p className="text-sm text-designColor tracking-[4px] uppercase">
             Features
           </p> */}
-          <h2 className="text-3xl md:text-4xl font-bold">Design Skill</h2>
+          <h2 className="text-3xl md:text-4xl font-bold">Technical & Technician Skill's</h2>
         </div>
         <div className='className="mt-14 w-full flex flex-col gap-6'>
           <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">Hardware Configuration</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-full h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">100%</span>
+              </motion.span>
+            </span>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">Software Configuration</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">90%</span>
+              </motion.span>
+            </span>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">Network Configuration</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[80%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">80%</span>
+              </motion.span>
+            </span>
+          </div>
+
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">Operating System Configuration</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[80%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">80%</span>
+              </motion.span>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      
+
+      <div className="w-full lgl:w-1/2">
+        <div className="py-12 font-titleFont flex flex-col gap-4">
+          {/* <p className="text-sm text-designColor tracking-[4px] uppercase">
+            Features
+          </p> */}
+          <h2 className="text-3xl md:text-4xl font-bold">Photo & Video Editing Skill's</h2>
+        </div>
+        <div className='className="mt-14 w-full flex flex-col gap-6'>
+            <div className="overflow-x-hidden">
             <p className="text-sm uppercase font-medium">Canva</p>
             <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
               <motion.span
@@ -69,18 +136,7 @@ const Skills = () => {
               </motion.span>
             </span>
           </div>
-        </div>
-      </div>
 
-
-      <div className="w-full lgl:w-1/2">
-        <div className="py-12 font-titleFont flex flex-col gap-4">
-          {/* <p className="text-sm text-designColor tracking-[4px] uppercase">
-            Features
-          </p> */}
-          <h2 className="text-3xl md:text-4xl font-bold">Video Editing Skill</h2>
-        </div>
-        <div className='className="mt-14 w-full flex flex-col gap-6'>
           <div className="overflow-x-hidden">
             <p className="text-sm uppercase font-medium">Adobe Premiere Pro</p>
             <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
@@ -128,7 +184,7 @@ const Skills = () => {
           {/* <p className="text-sm text-designColor tracking-[4px] uppercase">
             Features
           </p> */}
-          <h2 className="text-3xl md:text-4xl font-bold">Development Skill</h2>
+          <h2 className="text-3xl md:text-4xl font-bold">IT Programming & Development Skill's</h2>
         </div>
         <div className="flex flex-col gap-6">
           <div className="overflow-x-hidden">
@@ -210,7 +266,7 @@ const Skills = () => {
             </span>
           </div>
           <div className="overflow-x-hidden">
-            <p className="text-sm uppercase font-medium">SOFTWARE</p>
+            <p className="text-sm uppercase font-medium">JAVA LANGUAGE</p>
             <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
               <motion.span
                 initial={{ x: "-100%", opacity: 0 }}
@@ -219,6 +275,32 @@ const Skills = () => {
                 className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
               >
                 <span className="absolute -top-7 right-0">90%</span>
+              </motion.span>
+            </span>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">PYTHON LANGUAGE</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-full h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">100%</span>
+              </motion.span>
+            </span>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">DATABASE (MYSQL)</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-full h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">100%</span>
               </motion.span>
             </span>
           </div>
